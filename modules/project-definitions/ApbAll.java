f637d68e5f0a51a23b57b69c95edb7a35221f37e
Emilio Lopez-Gabeiras <emiliolg@mac.com>
2009-02-09 16:33:59 -0200

    Update

--- modules/project-definitions/ApbAll.java
+++ modules/project-definitions/ApbAll.java
@@ -1,18 +1,18 @@
 
-// ...........................................................................................................
-// (C) Copyright  1996/2008 Fuego Inc.  All Rights Reserved
-// THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Fuego Inc.
-// The copyright notice above does not evidence any actual or intended
-// publication of such source code.
-//
-// Last changed on 2008-12-10 20:02:43 (-0200), by: emilio. $Revision$
-// ...........................................................................................................
 
+// Copyright 2008-2009 Emilio Lopez-Gabeiras
 //
-// User: emilio
-// Date: Dec 9, 2008
-// Time: 5:35:09 PM
-
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
 //
 
 import apb.metadata.Project;
