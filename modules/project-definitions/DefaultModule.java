0f45e60c57b42d2b7d1b7c5ed99587e9e36d5771
Emilio Lopez-Gabeiras <emiliolg@mac.com>
2009-04-24 12:51:27 -0300

    update version number

--- modules/project-definitions/DefaultModule.java
+++ modules/project-definitions/DefaultModule.java
@@ -4,7 +4,7 @@ public class DefaultModule
         extends Module {
     {
 
-        version = "0.9.6";
+        version = "0.9.7";
 
         compiler.lint = true;
         compiler.failOnWarning = true;
