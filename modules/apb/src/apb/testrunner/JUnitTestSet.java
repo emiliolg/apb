b01cee09924d4a52496ccd403353d105d59b7c0d
Diego Rubinstein <diego.rubinstein@oracle.com>
2009-07-23 16:55:55 -0300

    skip JUnit Test classes when testGroups is not empty

--- modules/apb/src/apb/testrunner/JUnitTestSet.java
+++ modules/apb/src/apb/testrunner/JUnitTestSet.java
@@ -77,7 +77,11 @@ public final class JUnitTestSet
 
         // No suite build one
         if (suiteMethod == null) {
-            return new TestSuite(getTestClass());
+            //but only if no annotatios because can only annotate suites.
+            if(testGroups.isEmpty()){
+             return new TestSuite(getTestClass());
+            }
+            else return null;
         }
 
         // Check if I've to run it and run it
