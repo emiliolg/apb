3d76d16f0debcf224e6f9dcd11aa14f388f40aaa
Daniel Mario Vega <dv.github.n.dvega@xoxy.net>
2010-01-11 11:17:11 -0300

    Revert "Do not add compile-time dependecies to test-runner classpath"
    This reverts commit 9ba00e3cf2e88f6ac8f6fed76ce6061dc0e2d7e3.

--- modules/apb-base/src/apb/ModuleHelper.java
+++ modules/apb-base/src/apb/ModuleHelper.java
@@ -590,10 +590,7 @@ public class ModuleHelper
                 result.addAll(dependency.asModule().getHelper().deepClassPath(useJars, true));
             }
             else if (dependency.isLibrary()) {
-                final Library library = dependency.asLibrary();
-                if (library.mustInclude(false)) {
-                    addIfNotNull(result, library.getArtifact(this, PackageType.JAR));
-                }
+                addIfNotNull(result, dependency.asLibrary().getArtifact(this, PackageType.JAR));
             }
         }
 
